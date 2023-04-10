4d829ba8e49513a45409079fb181cc313a2a85a5
blue.pen5805 <blue.pen5805@fastmail.com>
2023-04-10 22:46:29 +0900

    プロンプト入力欄にスクロールバーが表示されるように

--- javascript/easy_wheel_edit_attention.js
+++ javascript/easy_wheel_edit_attention.js
@@ -12,3 +12,12 @@ addEventListener('wheel', (event) => {
 
   keyupEditAttention(newEvent);
 }, { passive: false });
+
+onUiLoaded(() => {
+  ['txt2img_prompt', 'txt2img_neg_prompt', 'img2img_prompt', 'img2img_neg_prompt'].forEach(id => {
+    const container = gradioApp().getElementById(id)
+    const textarea = container.querySelector('textarea')
+    textarea.classList.remove('scroll-hide')
+    textarea.style.overflowY = 'auto'
+  })
+})
